732facc682bcb071159c62e77afcc7da6c6fe7f6
wuyindi <wuyindi@xiaoniangao.com>
2018-12-09 22:56:38 +0800

    tabbar

--- GitHubPopular/index.ios.js
+++ GitHubPopular/index.ios.js
@@ -9,7 +9,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 
@@ -17,7 +18,7 @@ export default class GitHubPopular extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedTab: 'home',
+            selectedTab: 'tb_popular',
         }
     }
     render() {
@@ -25,20 +26,40 @@ export default class GitHubPopular extends Component {
             <View style={styles.container}>
                 <TabNavigator>
                     <TabNavigator.Item
-                        selected={this.state.selectedTab === 'home'}
-                        title="Home"
-                        renderIcon={() => <Image source={require('./res/images/ic_polular.png')} />}
-                        renderSelectedIcon={() => <Image source={require('./res/images/ic_polular.png')} />}
-                        onPress={() => this.setState({ selectedTab: 'home' })}>
-                        <View></View>
+                        selected={this.state.selectedTab === 'tb_popular'}
+                        selectedTitleStyle={{color:'red'}}
+                        title="最热"
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() =><Image style={[styles.image,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_popular'})}>
+                        <View style={{backgroundColor: 'red',flex:1}}></View>
                     </TabNavigator.Item>
                     <TabNavigator.Item
-                        selected={this.state.selectedTab === 'profile'}
-                        title="Profile"
-                        renderIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
-                        renderSelectedIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
-                        onPress={() => this.setState({ selectedTab: 'profile' })}>
-                        <View></View>
+                        selected={this.state.selectedTab === 'tb_trending'}
+                        title="趋势"
+                        selectedTitleStyle={{color:'yellow'}}
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() =><Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_trending'})}>
+                        <View style={{backgroundColor: 'yellow',flex:1}}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_favorite'}
+                        title="收藏"
+                        selectedTitleStyle={{color:'green'}}
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_favorite.png')}/>}
+                        renderSelectedIcon={() =><Image style={[styles.image,{tintColor:'green'}]} source={require('./res/images/ic_favorite.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_favorite'})}>
+                        <View style={{backgroundColor: 'green',flex:1}}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_my'}
+                        title="我的"
+                        selectedTitleStyle={{color:'blue'}}
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')}/>}
+                        renderSelectedIcon={() =><Image style={[styles.image,{tintColor:'blue'}]} source={require('./res/images/ic_my.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_my'})}>
+                        <View style={{backgroundColor: 'blue',flex:1}}></View>
                     </TabNavigator.Item>
                 </TabNavigator>
             </View>
@@ -49,8 +70,6 @@ export default class GitHubPopular extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   page1: {
@@ -60,6 +79,10 @@ const styles = StyleSheet.create({
   page2: {
     flex: 1,
     backgroundColor: 'yellow',
+  },
+  image: {
+      height: 22,
+      width: 22
   }
 });
 
